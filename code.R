afea7ff407df59162b6762c2aef5cfabf0e25066
katelyn-zhao <146498901+katelyn-zhao@users.noreply.github.com>
2024-10-20 19:12:07 -0700

    Create code.R

---
+++ code.R
@@ -0,0 +1,25 @@
+# Read txt file to data frame
+video_data <- read.table("videodata.txt", header = TRUE, sep="")
+video_multiple <- read.table("videoMultiple.txt", header = TRUE, sep="")
+
+#Question 1
+#Point Estimate Calculation
+point_estimate_fraction <- dim(video_data[videoData$time > 0,])[1] / dim(video_data)[1] #0.3736264
+
+#Interval Estimate Calculation
+estimate_error <- sqrt(point_estimate_fraction * (1- point_estimate_fraction)) / sqrt(n)
+lower_interval_estimate_fraction <- point_estimate_fraction - (2 * estimate_error) #0.2722014
+upper_interval_estimate_fraction <- point_estimate_fraction + (2 * estimate_error) #0.4750513
+
+#Question 2
+daily_gamers <- video_data[video_data$freq == 1,]
+weekly_gamers <- video_data[video_data$freq == 2,]
+monthly_gamers <- video_data[video_data$freq == 3,]
+semesterly_gamers <- video_data[video_data$freq == 4,]
+
+daily_point_estimate_fraction <- dim(daily_gamers[daily_gamers$time > 0,])[1] / dim(daily_gamers)[1] #0.7777778
+weekly_point_estimate_fraction <- dim(weekly_gamers[weekly_gamers$time > 0,])[1] / dim(weekly_gamers)[1] #0.8571429
+monthly_point_estimate_fraction <- dim(monthly_gamers[monthly_gamers$time > 0,])[1] / dim(monthly_gamers)[1] #0.1111111
+semesterly_point_estimate_fraction <- dim(semesterly_gamers[semesterly_gamers$time > 0,])[1] / dim(semesterly_gamers)[1] #0.04347826
+
+#Ouestion 3
