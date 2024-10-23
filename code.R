cc7548bb540af1e0632ceeaf96c9b60110ac8ad2
Katelyn Zhao <katelynzhao@Katelyns-MacBook-Pro-3.local>
2024-10-23 13:21:47 -0700

    update code.r

--- code.R
+++ code.R
@@ -4,10 +4,10 @@ video_multiple <- read.table("videoMultiple.txt", header = TRUE, sep="")
 
 #Question 1
 #Point Estimate Calculation
-point_estimate_fraction <- dim(video_data[video_data$time > 0,])[1] / dim(video_data)[1] #0.3736264
+point_estimate_fraction <- nrow(video_data[video_data$time > 0,]) / nrow(video_data) #0.3736264
 
 #Interval Estimate Calculation
-n <- dim(video_data)[1]
+n <- nrow(video_data)
 estimate_error <- sqrt(point_estimate_fraction * (1- point_estimate_fraction)) / sqrt(n)
 lower_interval_estimate_fraction <- point_estimate_fraction - (2 * estimate_error) #0.2722014
 upper_interval_estimate_fraction <- point_estimate_fraction + (2 * estimate_error) #0.4750513
@@ -18,10 +18,10 @@ weekly_gamers <- video_data[video_data$freq == 2,]
 monthly_gamers <- video_data[video_data$freq == 3,]
 semesterly_gamers <- video_data[video_data$freq == 4,]
 
-daily_point_estimate_fraction <- dim(daily_gamers[daily_gamers$time > 0,])[1] / dim(daily_gamers)[1] #0.7777778
-weekly_point_estimate_fraction <- dim(weekly_gamers[weekly_gamers$time > 0,])[1] / dim(weekly_gamers)[1] #0.8571429
-monthly_point_estimate_fraction <- dim(monthly_gamers[monthly_gamers$time > 0,])[1] / dim(monthly_gamers)[1] #0.1111111
-semesterly_point_estimate_fraction <- dim(semesterly_gamers[semesterly_gamers$time > 0,])[1] / dim(semesterly_gamers)[1] #0.04347826
+daily_point_estimate_fraction <- nrow(daily_gamers[daily_gamers$time > 0,]) / nrow(daily_gamers) #0.7777778
+weekly_point_estimate_fraction <- nrow(weekly_gamers[weekly_gamers$time > 0,]) / nrow(weekly_gamers) #0.8571429
+monthly_point_estimate_fraction <- nrow(monthly_gamers[monthly_gamers$time > 0,]) / nrow(monthly_gamers) #0.1111111
+semesterly_point_estimate_fraction <- nrow(semesterly_gamers[semesterly_gamers$time > 0,]) / nrow(semesterly_gamers) #0.04347826
 
 hist(daily_gamers$time, 
      main = "Daily Gamers", 
